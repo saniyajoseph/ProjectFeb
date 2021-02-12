31ddbd2410b18665113fa687c78db51e3dc44835
saniyajoseph <saniyajoseph30@gmail.com>
2021-02-12 19:25:30 +0530

    Completed

---
+++ src/org/test/Data.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Data {
+
+	public void data()
+	{
+		System.out.println("Data...");
+	}
+	public static void main(String[] args) {
+		Data d=new Data();
+		d.data();
+
+	}
+
+}
